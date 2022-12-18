33d760656e78bcf25157e6e23eee7300caf93fff
KorYme <maxime.corby.pro@gmail.com>
2022-12-18 23:52:09 +0100

    [ADD] : UI menu end

--- SlayTheLig/Assets/Scripts/FightSystem.cs
+++ SlayTheLig/Assets/Scripts/FightSystem.cs
@@ -184,13 +184,10 @@ public class FightSystem : MonoBehaviour
     public void WinLose(bool win)
     {
         currentFightStep = FightStep.EndFight;
-        if (win)
+        uiManager.DisplayEndMenu(win);
+        foreach (CardBehaviour item in cardManager.cardBehaviours)
         {
-            Debug.Log("Partie gagnée");
-        }
-        else
-        {
-            Debug.Log("Partie perdue");
+            item.enabled = false;
         }
     }
 
